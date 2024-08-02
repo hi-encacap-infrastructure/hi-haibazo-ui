30706f764634b27d5d538fc3ede907a8c244af93
RPG KhanhNK <khanhnk@reactplus.com>
2024-08-02 09:57:04 +0700

    ci: update semantic-release configs

--- release.config.mjs
+++ release.config.mjs
@@ -3,7 +3,11 @@
  */
 
 export default {
-  branches: ["main", { name: "develop", prerelease: true }],
+  branches: [
+    "main",
+    { name: "develop", prerelease: true, channel: "beta" },
+    { name: "staging", prerelease: true, channel: "rc" },
+  ],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
