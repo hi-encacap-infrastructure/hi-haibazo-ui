d79ef962154456c9e3825e4c2876e08552db3f48
RPG KhanhNK <khanhnk@reactplus.com>
2024-08-02 09:46:48 +0700

    ci: update semantic-release configs

--- release.config.mjs
+++ release.config.mjs
@@ -3,14 +3,16 @@
  */
 
 export default {
-    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', [
-        "@semantic-release/gitlab",
-        {
-          "gitlabUrl": "http://192.168.1.35/gitlab",
-          "assets": [
-            { "path": "storybook-static/**", "label": "Storybook" }
-          ]
-        }
-      ]],
-    
-}
+  branches: ["main", { name: "develop", prerelease: true }],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/gitlab",
+      {
+        gitlabUrl: "http://192.168.1.35/gitlab",
+        assets: [{ path: "storybook-static/**", label: "Storybook" }],
+      },
+    ],
+  ],
+};
