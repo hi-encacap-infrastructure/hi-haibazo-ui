9713e49ec391febb9ed96de46ce19baa761d3e9a
RPG KhanhNK <khanhnk@reactplus.com>
2024-08-07 16:18:10 +0700

    [feat] HBZ-1 publish as a npm package

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
+import typescript from '@rollup/plugin-typescript';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.ts'),
+      fileName: 'main',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [],
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: true,
+          declaration: true,
+          outDir: 'dist',
+        }),
+      ],
+    },
+  },
 });
