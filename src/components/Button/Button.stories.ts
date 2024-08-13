d902e77eba393999438285d324645fa8c2402ba5
Khanh Nguyen <khanh.nguyen@members.encacap.com>
2024-08-13 14:32:23 +0000

    chore(HBZ-101): update button label

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -32,13 +32,13 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Primary',
+    label: 'Button',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Secondary',
+    label: 'Button',
   },
 };
 
